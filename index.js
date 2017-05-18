fb0753204a0adbbcc6c266463029d014478d6020
simpart <taria.ki4m@gmail.com>
2017-05-19 08:35:37 +0900

    first

---
+++ index.js
@@ -0,0 +1,40 @@
+/**
+ * @file mofron-effect-dev/index.js
+ * @author simpart
+ */
+
+/**
+ * @class xxx
+ * @brief radius style effect class
+ */
+mofron.effect.xxx = class extends mofron.Effect {
+    
+    constructor (prm) {
+        try {
+            super();
+            this.name('xxx');
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    enable () {
+        try {
+            
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    disable () {
+        try {
+            
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
+module.exports = mofron.effect.xxx;
