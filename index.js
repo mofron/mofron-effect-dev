45a200b3733d5a34997c4bc9c6691744bfb88a27
simpart <simpart>
2020-01-02 11:18:50 +0900

    supported v1.1

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 /**
  * @file mofron-effect-dev/index.js
- * @author simpart
+ * @brief effect module template for developper
+ * @license MIT
  */
-const mf = require('mofron');
-
-mf.effect.xxx = class extends mf.Effect {
-    
-    constructor (prm) {
+module.exports = class extends mofron.class.Effect {
+    /**
+     * initialize effect
+     * 
+     * @param (mixed) 
+     *                key-value: effect config
+     * @short
+     * @type private
+     */
+    constructor (p1) {
         try {
             super();
-            this.name('xxx');
+            this.name("");      // please set effect modume name
+            this.shortForm(""); // please set short form parameter
+            
+            /* init config */
+            
+	    if (0 < arguments.length) {
+                this.config(p1);
+	    }
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
+    /**
+     * effect contents
+     * 
+     * @type private
+     */
     contents (cmp) {
         try {
-            
+            /* effect contents */
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
-module.exports = mf.effect.xxx;
+/* end of file */
